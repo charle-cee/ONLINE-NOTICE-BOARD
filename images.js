5ef49cc8c0c135c199d1a3315eef13bca5240613
GladsonChabuka <ychabuka@gmail.com>
2021-08-11 16:05:58 +0200

    commit upcoming events upload page

---
+++ images.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class images extends Component(){
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ upload.js
@@ -0,0 +1,18 @@
+
+
+const Upload = () => {
+    return (
+        <div>
+            <div className="container">
+                <form>
+                    <input type="text" />
+                    <input type="text" />
+                    <input type="text" placeholder="Enter announcement" />
+                    <button name="submit">Upload</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Upload
